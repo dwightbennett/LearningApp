494ec720dd625ff02f142658bfd94f264656de4b
Dwight Bennett <dwight.bennett@primusstudios.com>
2022-09-09 11:16:32 -0600

    first training module

--- LearningApp/MainPage.xaml.cs
+++ LearningApp/MainPage.xaml.cs
@@ -1,25 +1,55 @@
-﻿namespace LearningApp;
+﻿
+namespace LearningApp;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	string translatedNumber;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+    void TranslateButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+		var enteredNum = PhoneNumberText.Text;
+		translatedNumber = PhonewordTranslator.ToNumber(enteredNum);
+
+		if(!string.IsNullOrEmpty(translatedNumber))
+		{
+			CallButton.IsEnabled = true;
+			CallButton.Text = "Call " + translatedNumber;
+		}
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+		{
+			CallButton.IsEnabled = false;
+			CallButton.Text = "Call";
+		}
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    async void CallButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+		if(await DisplayAlert("Dial a Number", "Would you like to call " + translatedNumber + "?",
+			"Yes",
+			"No"))
+		{
+			try
+			{
+				if (PhoneDialer.Default.IsSupported)
+					PhoneDialer.Default.Open(translatedNumber);
+			}
+			catch(ArgumentNullException)
+			{
+				await DisplayAlert("Unable to dial", "Phone number was not valid", "Ok");
+			}
+			catch(Exception ex)
+			{
+				await DisplayAlert("Unable to dial", "Phone Dialing failed", "Ok");
+			}
+		}
+    }
 }
 
 
