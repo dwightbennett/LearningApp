9fe3d7c6b100de4893d2bdec509a7c6730179d16
Dwight Bennett <dwight.bennett@primusstudios.com>
2022-09-12 11:50:49 -0600

    Lesson 2

---
+++ exercise1/Notes/SharedResource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Notes
+{
+	public static class SharedResource
+	{
+		public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+
+		public static readonly Color BackgroundColor = Color.FromRgb(0xFF, 0xF0, 0xAD);
+
+
+
+
+	}
+}
+
